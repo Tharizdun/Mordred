480153ef6ecc22b2de448836dd7798da32f0ed8f
Tharizdun <32910456+Tharizdun@users.noreply.github.com>
2018-11-25 22:24:33 +0100

    0.01

---
+++ experiments/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+	<meta charset="utf-8">
+	<title>úvodní obrazovka</title>
+	<link rel="stylesheet" href="styl.css" type="text/css">
+</head>
+<body>
+
+
+<div id="login">
+<form method="post" action="login.php" id="form">
+	<div class="form">
+		<p>E-mail</p>
+		<input type="email" class="form-item" name="email">
+	</div>
+
+	<div class="form">
+		<p>Password</p>
+		<input type="password" class="form-item" name="password">
+	</div>
+    
+	<div class="form-button">
+		<input type="submit" class="form-item" value="Log-in">
+	</div>
+</form>
+</div>
+
+
+</body>
+</html>
---
+++ experiments/testing.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+	<meta charset="utf-8">
+	<title>úvodní obrazovka</title>
+	<link rel="stylesheet" href="styl.css" type="text/css">
+</head>
+<body>
+
+
+<div id="okno">
+	<div class="prvky"></div>
+	<div class="prvky"></div>
+	<div class="prvky"></div>
+	<div class="prvky"></div>
+	<div class="prvky"></div>
+	<div class="prvky"></div>
+	<div class="prvky"></div>
+	<div class="prvky"></div>
+	<div class="prvky"></div>
+	<div class="prvky"></div>
+	<div class="prvky"></div>
+</div>
+
+
+</body>
+</html>
