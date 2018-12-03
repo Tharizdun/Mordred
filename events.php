f26611cc9db3c48cc65e24e3bd0b9cc1af202d95
Tharizdun <32910456+Tharizdun@users.noreply.github.com>
2018-12-03 19:24:41 +0100

    events fix

--- events.php
+++ events.php
@@ -263,6 +263,14 @@ if ($showSuccess)
                 <span>Time</span>
             </div>
             
+            <div class="PlaceTitle">
+                <span>Place</span>
+            </div>
+            
+            <div class="Place">
+                <p>Brno hlavni nadrazi</p>
+            </div>
+            
             <div class="DescriptionTitle">
                 <span>Description</span>
             </div>
@@ -270,6 +278,8 @@ if ($showSuccess)
             <div class="description">
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
             </div>
+            
+            
             
             <div class="DescriptionAttendees">
                 <span>Attendees</span> 
